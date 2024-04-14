8f4296849c2d26ad8719721fd3ec27a5ba00ddc7
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-04-13 22:14:53 -0500

    Declare api call and App states

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -1,4 +1,15 @@
 const getCharacters = async () => {
+    try {
+        const response = await fetch('https://stardew-api.onrender.com/api/v1/characters');
+        if (!response.ok) {
+            const status = response.status
+            throw new Error(`We couldn't get the characters - ${status}`)
+        }
+        return await response.json();
+    } catch (error: any) {
+        console.log(error.message)
+        throw error
+    }
 
 }
 
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,9 +6,35 @@ import Profile from '../Profile/Profile';
 import Besties from '../Besties/Besties';
 import Error from '../Error/Error';
 import { Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { getCharacters } from '../../apiCalls';
 
 
 function App() {
+  const [error, setError] = useState<any>('');
+  const [characters, setCharacters] = useState<Character[]>([]);
+
+  type Character = {
+    id: string
+    name: string
+    birthday: string
+    hobbies: string[]
+    avatar: string
+  }
+
+  useEffect(() => {
+    fetchCharacters();
+  }, [])
+
+  const fetchCharacters = async () => {
+    try {
+      const characters = await getCharacters()
+      setCharacters(characters)
+    } catch(error) {
+        setError(`${error}`)
+    }
+  }
+
   return (
     <div className="App">
       <Nav />
