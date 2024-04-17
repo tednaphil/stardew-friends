57d16df055321d3915e2835fd690c10665d94fbc
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-04-16 23:46:07 -0500

    Add counter to Besties nav link

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,13 +1,15 @@
 import './Nav.css';
 import { NavLink, useLocation } from 'react-router-dom';
 import Search from '../Search/Search';
+import type { Friend } from '../App/App';
 
 interface Props {
     search: string
     setSearch: (query: string) => void
+    besties: Friend[]
 }
 
-function Nav({search, setSearch}: Props) {
+function Nav({search, setSearch, besties}: Props) {
     const { pathname } = useLocation()
     return (
         <header className='header'>
@@ -15,7 +17,7 @@ function Nav({search, setSearch}: Props) {
                 <h1 className='heading'>Stardew Friends</h1>
                 <section className='links'>
                     <NavLink to='/' id='home-link' >Home</NavLink>
-                    <NavLink to='/besties' id='besties-link' className='vl'>Besties</NavLink>
+                    <NavLink to='/besties' id='besties-link' className='vl'>{`Besties (${besties.length})`}</NavLink>
                 </section>
                 {pathname === '/' && <Search search={search} setSearch={setSearch}/>}
             </nav>
