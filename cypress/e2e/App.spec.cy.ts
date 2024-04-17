a20bf73cb89b675c551c8bd14808b712d9f85e0d
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-04-17 12:45:34 -0500

    Add homepage display assertions

--- cypress/e2e/App.spec.cy.ts
+++ cypress/e2e/App.spec.cy.ts
@@ -12,25 +12,39 @@ describe('Stardew Friends User Stories', () => {
       }).as('getCharacter')
   })
   it('Displays Homepage', () => {
-    cy.visit('https://stardew-friends.vercel.app/')
+    cy.visit('http://localhost:3000/')
+    .get('.loading-screen').contains('h2', 'Loading...')
+    .get('.loading-image').should('have.attr', 'src')/*.should('include', 'data:image/gif')*/
     .wait('@getCharacters')
-    //check loaded content
+    .get('.nav-bar').children().should('have.length', 3)
+    .get('.heading').contains('Stardew Friends')
+    .get('.links').contains('a', 'Home')
+    .get('.links').contains('a', 'Besties')
+    .get('.search-bar').should('exist')
+    .get('.character-cards').children().should('have.length', 4)
+    .get('.card-wrapper').first().contains('p', 'Sandy')
+    .get('.char-avatar').first().should('have.attr', 'alt').should('equal', 'Sandy avatar')
+    .get('.card-wrapper').last().contains('p', 'Wizard')
+    .get('.char-avatar').last().should('have.attr', 'alt').should('equal', 'Wizard avatar')
   })
-  it('Shows individual character profile', () => {
-    //click character from homepage
-    //check content
-  })
-  it('Adds and removes besties', () => {
-    //add bestie from character profile
-    //check besties view
-    //remove bestie
-    //check bestie view
+  // it('Shows individual character profile', () => {
+  //   //click character from homepage
+  //   //check content
+  // })
+  // it('Adds and removes besties', () => {
+  //   //add bestie from character profile
+  //   //check besties view
+  //   //remove bestie
+  //   //check bestie view
+  // })
+  // it('Increments frienship level', () => {
 
-  })
-  it('Displays search results', () => {
-    //check value of search input
-    //type search
-    //check displayed results
+  // })
 
-  })
+  // it('Displays search results', () => {
+  //   //check value of search input
+  //   //type search
+  //   //check displayed results
+
+  // })
 })
